3e2bff9b995259ca50f20566910c5d3f38da88be
TheLitis <lovalentin08@gmail.com>
2026-01-18 11:45:46 +0000

    fix(web): wrap app with TooltipProvider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
+import { TooltipProvider } from "./components/ui/tooltip";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <TooltipProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TooltipProvider>
     </AuthProvider>
   </React.StrictMode>
 );
